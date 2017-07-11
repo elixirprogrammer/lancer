becdd41f9fe05cce715fea020644702803113db3
boilercoding <boilercoding@gmail.com>
2017-07-11 17:16:49 -0400

    Adds conditional function to users controller

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,38 +3,9 @@ defmodule Lancer.UserController do
 
   alias Lancer.User
 
-  def index(conn, params) do
-    render conn, :index
-  end
-
   def new(conn, _) do
     changeset = User.changeset(%User{})
     render conn, :new, changeset: changeset
-  end
-
-  def edit(conn, %{"id" => id}) do
-    user = Repo.get(User, id)
-    if conn.assigns.current_user == user do
-      changeset = User.changeset(user)
-      render(conn, :edit, user: user, changeset: changeset)
-    else
-      conn
-      |> put_flash(:error, "You are not the owner of that user")
-      |> redirect(to: page_path(conn, :index))
-    end
-  end
-
-  def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Repo.get!(User, id)
-    changeset = User.reg_changeset(user, user_params)
-    case Repo.update(changeset) do
-      {:ok, user} ->
-        conn
-        |> put_flash(:info, "#{user.name} updated successfully.")
-        |> redirect(to: page_path(conn, :index))
-      {:error, changeset} ->
-        render(conn, :edit, user: user, changeset: changeset)
-    end
   end
 
   def create(conn, %{"user" => user_params}) do
@@ -50,4 +21,32 @@ defmodule Lancer.UserController do
     end
   end
 
+  def edit(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
+    authenticate_current_user_can(conn, user)
+    changeset = User.changeset(user)
+    render(conn, :edit, user: user, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Repo.get!(User, id)
+    authenticate_current_user_can(conn, user)
+    changeset = User.reg_changeset(user, user_params)
+    case Repo.update(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} updated successfully.")
+        |> redirect(to: page_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, :edit, user: user, changeset: changeset)
+    end
+  end
+
+  defp authenticate_current_user_can(conn, user) do
+    unless conn.assigns.current_user == user do
+      conn
+      |> put_flash(:error, "You are not the owner of that user")
+      |> redirect(to: page_path(conn, :index))
+    end
+  end
 end
