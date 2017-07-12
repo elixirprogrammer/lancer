3d3b23964f4bc586632ff77c5a64887fc28a6380
boilercoding <boilercoding@gmail.com>
2017-07-12 18:44:05 -0400

    Adds seeds with faker

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Lancer.Mixfile do
   def application do
     [mod: {Lancer, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :hasher, :number, :kerosene]]
+                    :phoenix_ecto, :postgrex, :hasher, :number, :kerosene, :faker]]
   end
 
   # Specifies which paths to compile per environment.
@@ -40,7 +40,8 @@ defmodule Lancer.Mixfile do
      {:cowboy, "~> 1.0"},
      {:hasher, "~> 0.1.0"},
      {:number, "~> 0.5.1"},
-     {:kerosene, "~> 0.7.0"}]
+     {:kerosene, "~> 0.7.0"},
+     {:faker, "~> 0.8"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,59 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Lancer.{Repo, User, Project, Category, Ability, Skill, Proposal}
+
+Repo.delete_all User
+(1..10) |> Enum.each(fn n ->
+  User.reg_changeset(%User{}, %{
+                        name: Faker.Name.title,
+                        email: Faker.Internet.free_email,
+                        username: Faker.Internet.user_name,
+                        password: "password123",
+                        password_confirmation: "password123"})
+  |> Repo.insert!()
+end)
+
+Repo.delete_all Category
+Category.changeset(%Category{}, %{name: "Web Development"})
+|> Repo.insert!()
+
+Category.changeset(%Category{}, %{name: "Design"})
+|> Repo.insert!()
+
+Category.changeset(%Category{}, %{name: "Finance"})
+|> Repo.insert!()
+
+Category.changeset(%Category{}, %{name: "Engineering"})
+|> Repo.insert!()
+
+Category.changeset(%Category{}, %{name: "Sales"})
+|> Repo.insert!()
+
+Category.changeset(%Category{}, %{name: "Administrative"})
+|> Repo.insert!()
+
+Repo.delete_all Project
+(1..50) |> Enum.each(fn n ->
+  Project.changeset(%Project{}, %{
+                        name: Faker.Name.title ,
+                        description: Faker.Lorem.paragraph(%Range{first: 1, last: 40}),
+                        budget: Enum.random(1000..20000),
+                        location: "#{Faker.Address.city}, #{Faker.Address.state_abbr}",
+                        user_id: :rand.uniform(10),
+                        category_id: :rand.uniform(6)})
+  |> Repo.insert!()
+end)
+
+Repo.delete_all Skill
+(1..100) |> Enum.each(fn n ->
+  Skill.changeset(%Skill{}, %{name: Faker.Superhero.power})
+  |> Repo.insert!()
+end)
+
+Repo.delete_all Ability
+(1..200) |> Enum.each(fn n ->
+  Ability.changeset(%Ability{}, %{
+    project_id: :rand.uniform(50), skill_id: :rand.uniform(100)})
+  |> Repo.insert!()
+end)
